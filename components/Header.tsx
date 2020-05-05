1f4c848f9fe6e045f10bc03f597db6b406abd62f
Aude Faucheux <aude53@gmail.com>
2020-05-05 19:21:37 +0100

    attempt to apply global style

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,6 +13,10 @@ const Header = () => {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
       />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Istok+Web:wght@700&display=swap"
+        rel="stylesheet"
+      />
       <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@0.4.0/dist/confetti.browser.min.js"></script>
     </Head>
   );
