66820139cdc22901499c55ef68db321b76d5984b
Aude Faucheux <aude53@gmail.com>
2020-05-07 21:35:14 +0100

    create API requests

--- Adapters/APIs.ts
+++ Adapters/APIs.ts
@@ -1,20 +1,26 @@
 import axios from "axios";
-
-enum theme {
-  SLOTH = "sloth",
-  CAT = "cat",
-  DUCK = "duck",
-  GEORGE = "george",
-  KEANU = "keanu",
-  SOUTH_PARK = "south park",
-}
-
-interface User {
-  username: string;
-  theme: theme;
-  credit: number;
-}
+import { User } from "../interfaces/User";
 
 export const getUsers = (): Promise<User[]> => {
-  return axios.get("http://localhost:3004/users").then((res) => res.data);
+  return axios({
+    method: "get",
+    url: "http://localhost:3004/users",
+  }).then((res) => res.data);
+};
+
+export const createUser = (data: User): Promise<User[]> => {
+  console.log(data);
+  return axios({
+    method: "post",
+    url: "http://localhost:3004/users",
+    data,
+  }).then((res) => res.data);
+};
+
+export const updateUser = (id: string, data: User): Promise<User[]> => {
+  return axios({
+    method: "patch",
+    url: `http://localhost:3004/users/${id}`,
+    data,
+  }).then((res) => res.data);
 };
