d47ac38b371b1452baf4dd9bbeb2ef3085b30bb7
Aude Faucheux <aude53@gmail.com>
2020-05-09 14:57:18 +0100

    create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div id="stop-logo">
+      <img
+        src="./images/game/when-the-fun-stops-stop.jpg"
+        alt="When The Fun Stops, Stop"
+      />
+    </div>
+  );
+};
+
+export default Footer;
